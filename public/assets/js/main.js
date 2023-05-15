1fc35d05bdbf5d5b7dd8352d694374777a899fee
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-05-15 13:37:25 +0200

    gedrfsfd

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20,9 +20,10 @@ function requestPermission () {
 }
 requestPermission();
 function update () {
+    document.getElementById("test-values").innerHTML = "HELP!";
     can.clear();
     can.setColour(new Colour(0,0,0));
-    document.getElementById("test-values").innerHTML = new VECTOR.Vector2(Math.round(tiltValue.y+250),Math.round(tiltValue.z+250));
+    //document.getElementById("test-values").innerHTML = new VECTOR.Vector2(Math.round(tiltValue.y+250),Math.round(tiltValue.z+250));
     can.drawCircle(new VECTOR.Vector2(tiltValue.y+250,tiltValue.z+250), 50);
     requestAnimationFrame(update);
 }
