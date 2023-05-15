dd3f5db3996018f483b9d6c3994afa9a6639afa9
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-05-15 14:50:09 +0200

    fix please??

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -23,8 +23,7 @@ requestPermission();
 function update () {
     can.clearCanvas();
     can.setColour(new Colour(0,0,0));
-    const ballPos = new VECTOR.Vector2(Math.round(tiltValue.z+250),Math.round(tiltValue.y+250));
-    document.getElementById("test-values").innerHTML = ballPos + Math.random();
-    can.drawCircle(ballPos, 50);
+    document.getElementById("test-values").innerHTML = new VECTOR.Vector2(Math.round(tiltValue.z+250),Math.round(tiltValue.y+250));
+    can.drawCircle(new VECTOR.Vector2(tiltValue.z+250,tiltValue.y+250), 50);
     requestAnimationFrame(update);
 }
