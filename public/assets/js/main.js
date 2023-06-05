4f4bc97b0de53b7df6fcf165a63f799c74cb5bd3
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-06-05 13:10:34 +0200

    got it to work

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -29,6 +29,6 @@ function update () {
     can.clearCanvas();
     can.setColour(new Colour(0,0,0));
     //document.getElementById("test-values").innerHTML = new VECTOR.Vector2(Math.round(tiltValue.y+250),Math.round(tiltValue.z+250));
-    can.drawCircle(new VECTOR.Vector2(tiltValue.y+250,tiltValue.z+250), 50);
+    can.drawCircle(new VECTOR.Vector2(tiltValue.z+250,tiltValue.y+250), 50);
     requestAnimationFrame(update);
 }
