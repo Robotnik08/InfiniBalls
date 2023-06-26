a562952a134bc9c8de9035ae86c01312b2f21cc0
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-06-26 12:25:39 +0200

    dewwff

--- public/game/assets/js/main.js
+++ public/game/assets/js/main.js
@@ -36,7 +36,7 @@ function update () {
     main.gravity = new Vector2(tiltValue.z/250,tiltValue.y/250);
     requestAnimationFrame(update);
 }
-const amount = 350;
+const amount = getQueryStringValue("num-balls");
 for (let i = 0; i < amount; i++) {
     main.addPoint(env.getPoint(env.getRandomPosition(size),new Colour(0,255,255,1),15,0,1));
     //main.addShape(env.getWheel(4,90,new Vector2(100,100),new Colour(0,0,0,1),1,0.9,0.5));
